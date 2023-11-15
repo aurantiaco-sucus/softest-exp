026909da1435b2a690183958cc2c751dca5a5ca4
AurantiacoSucus <30406783+aurantiaco-sucus@users.noreply.github.com>
2023-11-15 11:51:47 +0800

    23.11.15-1

--- java-gradle-junit/src/test/java/cn/edu/gzhu/DateUtilTest.java
+++ java-gradle-junit/src/test/java/cn/edu/gzhu/DateUtilTest.java
@@ -6,13 +6,19 @@ import org.junit.jupiter.api.Test;
 class DateUtilTest {
 
     @Test
-    void testIsLeapYear() {
+    void testIsLeapYear() { // dividable by 4, by both 100 and 400, but not just 100 and not 400
         Assertions.assertTrue(DateUtil.isLeapYear(4));
+        Assertions.assertTrue(DateUtil.isLeapYear(8));
+        Assertions.assertTrue(DateUtil.isLeapYear(400));
+        Assertions.assertTrue(DateUtil.isLeapYear(800));
+        Assertions.assertFalse(DateUtil.isLeapYear(1));
+        Assertions.assertFalse(DateUtil.isLeapYear(5));
+        Assertions.assertFalse(DateUtil.isLeapYear(200));
+        Assertions.assertFalse(DateUtil.isLeapYear(600));
     }
 
     @Test
     void testGetDayofYear() {
-        int result = DateUtil.getDayofYear(new Date(0, 0, 0));
-        Assertions.assertEquals(0, result);
+        Assertions.assertEquals(DateUtil.getDayofYear(new Date(0, 0, 0)), 0);
     }
 }
